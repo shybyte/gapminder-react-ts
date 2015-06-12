b0d9312e2614afabf3bf81d142bbb85f3fb87eb9
shybyte <ein@volloeko.de>
2015-06-12 13:51:59 +0200

    Use ReactJs and Ramda

--- src/scripts/gapminder.ts
+++ src/scripts/gapminder.ts
@@ -1,6 +1,23 @@
-function test(s: string) {
-  var [x, y] = [10, 20];
-  return s + ' test:';
+module gapminder {
+  'use strict';
+
+  var div = React.createFactory('div');
+
+
+  interface MainComponentProps {
+
+  }
+
+  class MainComponent extends React.Component<MainComponentProps,any> {
+    render() {
+      return div({}, "Hello World")
+    }
+  }
+
+  React.render(React.createElement(MainComponent), document.getElementById('app'));
+  console.log(R.filter(x => x > 5, [1]));
+
 }
 
-console.log(test("Yeah"));
+
+
